4ddc0d9c09f79b5555cb3e66add54c73e3bd08bf
furno west <furnowest57@gmail.com>
2020-04-11 16:48:04 -0700

    added a click

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+// // Your code goes here
+//  document.querySelectorAll(".btn")
+// button.addEventListener("click", (el) =>{
+// el.style.backgroundColor = "yellow"
+// });
+document.querySelectorAll(".btn").forEach(element => {
+    element.addEventListener("click", () => {
+        element.style.backgroundColor="pink"
+        // if there r few its better to use for each to go around
+    })
+    
+});
+
+
